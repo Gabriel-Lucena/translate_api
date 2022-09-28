f6672a79ac837cc1cdb4ed2663fc5faf194021cf
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2022-09-27 22:27:08 -0300

    Resolução de Erros do Heroku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 const translateController = require('./controller/translate');
 app.use('/', translateController);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is listening on port 3000');
 });
